bcfb975874be309a83b2b386590a1efc89f7c749
Tyler Brooks <tylerbrooksmedia@gmail.com>
2014-04-09 14:57:49 -0600

    Implemented Devise for Users

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.1.0.rc2'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Tells Pinteresting what URL to use for emails
+  #Required for Heroku
+  #Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'almostpinterest.com' }
 end
