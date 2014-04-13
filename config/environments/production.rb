8fcfab7151921f31fd214f7dcb6e814581ea7fb9
Tyler Brooks <tylerbrooksmedia@gmail.com>
2014-04-12 21:48:48 -0600

    Update show view, add glyphicons, and change root path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,11 @@ Pinteresting::Application.configure do
 
   #Sets Paperclip to upload images to AWS S3
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 end
